4cf7fe24083355ee5dd3320803c96d060df840b5
Peter Kuhmann <info@peter-kuhmann.de>
2023-07-21 23:59:05 +0200

    Changed editor tab scrolling

--- src/components/project/tabs/EditProjectTab.tsx
+++ src/components/project/tabs/EditProjectTab.tsx
@@ -10,9 +10,9 @@ export default function EditProjectTab({
   children,
 }: EditProjectTabProps) {
   return (
-    <div className={"h-full p-4 w-[18rem] flex flex-col"}>
+    <div className={"h-full p-4 w-[18rem] flex flex-col overflow-y-auto"}>
       <h1 className={"text-xl font-semibold mb-8"}>{title}</h1>
-      <div className={"flex-grow h-1 overflow-y-auto"}>{children}</div>
+      <div className={""}>{children}</div>
     </div>
   );
 }
