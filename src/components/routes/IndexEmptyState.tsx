6f03ae7d8f033600588c494410fe6beecb7f0a50
Peter Kuhmann <info@peter-kuhmann.de>
2023-07-23 17:47:07 +0200

    Clipboard paste hint in empty state

--- src/components/routes/IndexEmptyState.tsx
+++ src/components/routes/IndexEmptyState.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import CuteCat from "@/components/cat/CuteCat.tsx";
 import { getCreateProjectLink } from "@/navigation/links.ts";
 import IonIcon from "@/components/IonIcon.tsx";
+import SKC from "@/components/SKC.tsx";
 
 export interface IndexEmptyStateProps {
   onImport: () => void;
@@ -12,13 +13,10 @@ export default function IndexEmptyState({ onImport }: IndexEmptyStateProps) {
 
   return (
     <div className={"flex flex-col items-center"}>
-      <CuteCat className={"w-full max-w-[10rem] mb-8"} />
+      <CuteCat className={"w-full max-w-[10rem] mb-12"} />
 
       <div className={"text-2xl mb-16 text-center leading-relaxed"}>
-        <br />
         <b>Let's create your first project!</b>
-        <br />
-        (or drop an export file here)
       </div>
 
       <button
@@ -39,6 +37,13 @@ export default function IndexEmptyState({ onImport }: IndexEmptyStateProps) {
         ... or import an export
         <IonIcon name={"cloud-upload-outline"} />
       </button>
+
+      <div className={"my-4"}>OR</div>
+
+      <div className={"text-xl mb-4 text-center leading-relaxed"}>
+        Paste image via <SKC small keys={["#PCTRL", "V"]} />,<br /> drop image
+        file or Pan'n'Zoom export.
+      </div>
     </div>
   );
 }
