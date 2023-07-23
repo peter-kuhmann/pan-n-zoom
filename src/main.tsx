39044f7b67ed2ca4654e46265d4aa2010a3db288
Peter Kuhmann <info@peter-kuhmann.de>
2023-07-23 18:52:04 +0200

    Opening PWA from root → redirect to /app

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./main.scss";
 import Router from "./Router.tsx";
 import "@fontsource-variable/shantell-sans";
+import { reloadAppIfStandaloneModeChanges } from "@/utils/standalone.ts";
+
+reloadAppIfStandaloneModeChanges();
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
--- src/utils/standalone.ts
+++ src/utils/standalone.ts
@@ -1,7 +1,16 @@
+export const IsStandaloneMediaQuery = "(display-mode: standalone)";
+
 export const isRunningStandalone: boolean = (function () {
   try {
-    return window.matchMedia("(display-mode: standalone)").matches;
+    return window.matchMedia(IsStandaloneMediaQuery).matches;
   } catch (e) {
     return false;
   }
 })();
+
+export function reloadAppIfStandaloneModeChanges() {
+  const mql = window.matchMedia(IsStandaloneMediaQuery);
+  mql.onchange = () => {
+    window.location.reload();
+  };
+}
